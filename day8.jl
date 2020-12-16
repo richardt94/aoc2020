13f7c232db4b77b6e1e79a0ba807707ba5ab5ff1
richardt94 <29562583+richardt94@users.noreply.github.com>
2020-12-16 11:46:18 +1100

    partial day9

--- day8.jl
+++ day8.jl
@@ -15,15 +15,19 @@ function emulate(filename::String)
 		if exec[console.ip + 1]
 			return console.acc
 		end
+		print("executing instruction ",console.ip," ")
 		exec[console.ip + 1] = true
 		instr = instructions[console.ip + 1]
 		i, arg = instr[1], parse(Int, instr[2])
 		if i == "acc"
+			println("acc")
 			console.acc += arg
 			console.ip += 1
 		elseif i == "jmp"
+			println("jmp")
 			console.ip += arg
 		elseif i == "nop"
+			println("nop")
 			console.ip += 1
 		end
 	end
---
+++ day9.jl
@@ -0,0 +1,62 @@
+function sum_code(lp::Int, numbers::Array{Int,1})
+	#construct the set of possible sums
+	sums = Dict{Int,Int}()
+
+	for i = 1:lp
+		for j = i+1:lp
+			s = numbers[i]+numbers[j]
+			if haskey(sums, s)
+				sums[s] = sums[s] + 1
+			else
+				sums[s] = 1
+			end
+		end
+	end
+
+	for i = lp+1:length(numbers)
+		newn = numbers[i]
+		if !haskey(sums, newn)
+			return newn
+		end
+		#remove the expired element from the set
+		#and add the new one
+		expired = numbers[i-lp]
+		for j = (i-lp+1):(i-1)
+			sums[expired + numbers[j]] -= 1
+			if haskey(sums, newn + numbers[j])
+				sums[newn+numbers[j]] += 1
+			else
+				sums[newn+numbers[j]] = 1
+			end
+			if sums[expired + numbers[j]] == 0
+				delete!(sums, expired + numbers[j])
+			end
+		end
+
+	end
+
+	return 0
+
+end
+
+function sumtok(s::Int, numbers::Array{Int,1})
+	cumsums = Dict{Int,Int}()
+	cs = 0
+	for (i,n) = enumerate(numbers)
+		cs += n
+		cumsums[cs] = i
+	end
+
+	for cusum = keys(cumsums)
+		if haskey(cumsums, cusum-s) && (cumsums[cusum-s] <= cumsums[cusum] - 2)
+			return numbers[cumsums[cusum-s]] + numbers[cumsums[cusum]]
+		end
+	end
+	return 0
+end
+
+function breakxmas(lp::Int, filename::String)
+	numbers = parse.(Int, readlines(filename))
+	s = sum_code(lp, numbers)
+	sumtok(s, numbers)
+end
