72b514c0205c2f37ebc5a347453e03e7a183f141
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2020-12-16 16:08:28 +1100

    complete day 8

--- day8.jl
+++ day8.jl
@@ -7,13 +7,17 @@ Console() = Console(0,0)
 
 function emulate(filename::String)
 	instructions = split.(readlines(filename))
+	emulate(instructions)
+end
+
+function emulate(instructions::Array{Array{T,1},1} where T<:AbstractString)
 	console = Console()
 	halt = false
 
 	exec = zeros(Bool, length(instructions))
 	while console.ip < length(instructions)
 		if exec[console.ip + 1]
-			return console.acc
+			return false, console.acc
 		end
 		print("executing instruction ",console.ip," ")
 		exec[console.ip + 1] = true
@@ -32,6 +36,32 @@ function emulate(filename::String)
 		end
 	end
 
-	return console.acc
+	return true, console.acc
+end
 
+function trychangejmp(filename::String)
+	instructions = split.(readlines(filename))
+	trychangejmp(instructions)
+end
+
+function trychangejmp(instructions::Array{Array{T,1},1} where T<:AbstractString)
+	for i = 1:length(instructions)
+		instr = instructions[i]
+		if instr[1] == "jmp"
+			instr[1] = "nop"
+			halted, acc = emulate(instructions)
+			if halted
+				return i, acc
+			end
+			instr[1] = "jmp"
+		elseif instr[1] == "nop"
+			instr[1] = "jmp"
+			halted, acc = emulate(instructions)
+			if halted
+				return i, acc
+			end
+			instr[1] = "nop"
+		end
+	end
+	return i, 0
 end
