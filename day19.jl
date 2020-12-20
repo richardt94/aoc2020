e68b588a43baed8c440eed62ce234763fe149c4f
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2020-12-20 17:39:41 +1100

    solve day 19 part 2

--- day19.jl
+++ day19.jl
@@ -8,7 +8,7 @@ Rule(c::Char) = Rule(Array{Array{Int,1},1}(), c)
 Rule(subrules::Array{Array{Int,1},1}) = Rule(subrules, 'a')
 
 function buildGraph(rules::Array{T,1} where T<:AbstractString)
-    graph = [Rule() for _ in 1:length(rules)]
+    graph = Dict{Int,Rule}() #[Rule() for _ in 1:length(rules)]
     internalrgx = r"^([0-9]+): ([0-9 |]+)$"
     leafrgx = r"^([0-9]+): \"([a-z])\"$"
     for rule = rules
@@ -27,29 +27,71 @@ function buildGraph(rules::Array{T,1} where T<:AbstractString)
     graph
 end
 
-function matchrule(message::AbstractString, rulegraph::Array{Rule,1}, root::Int)
-    if length(message) == 0; return false, "" end
-    matched = false
+#hard-code looped matching for rules 8 and 11 (9 and 12 in our one-based indexing)
+#returns possible remainders of the string after a match is found. If no match is found
+#then the set of possible remainders will be empty.
+function matchrule(message::AbstractString, rulegraph::Dict{Int,Rule}, root::Int)
+    newrems = Set{String}()
+    if length(message) == 0; return newrems end
     if length(rulegraph[root].subrules) == 0
         if message[1] == rulegraph[root].rule
-            return true, message[2:end]
+            return Set{String}([message[2:end]])
         else
-            return false, ""
+            return Set{String}()
         end
-	else
+	elseif root == 9
+        #rule 8 expands to "at least one copy of rule 42"
+        remainders = matchrule(message, rulegraph, 43)
+        newrems = remainders
+        while length(remainders) > 0
+            nextrems = Set{String}()
+            for remainder = remainders
+                nextrems = union(nextrems, matchrule(remainder, rulegraph, 43))
+            end
+            newrems = union(newrems, nextrems)
+            remainders = nextrems
+        end
+    elseif root == 12
+        #"at least one copy of 42 followed by the same number of copies of 31"
+        n42rems = Array{Set{String},1}()
+        remainders = matchrule(message, rulegraph, 43)
+        push!(n42rems, remainders)
+        while length(remainders) > 0
+            nextrems = Set{String}()
+            for remainder = remainders
+                nextrems = union(nextrems, matchrule(remainder, rulegraph, 43))
+            end
+            push!(n42rems, nextrems)
+            remainders = nextrems
+        end
+
+        for (n42, rems31) = enumerate(n42rems)
+            currems = rems31
+            for n31 = 1:n42
+                nextrems = Set{String}()
+                for remainder = currems
+                    nextrems = union(nextrems, matchrule(remainder, rulegraph, 32))
+                end
+                currems = nextrems
+            end
+            newrems = union(newrems, currems)
+        end
+
+    else
         for subrule in rulegraph[root].subrules
             matched = true
-            remainder = message
+            remainders = Set{String}([message])
             for component in subrule
-                (matched, remainder) = matchrule(remainder, rulegraph, component)
-                if !matched; break end
+                nextrems = Set{String}()
+                for remainder = remainders
+                    nextrems = union(nextrems, matchrule(remainder, rulegraph, component))
+                end
+                remainders = nextrems
             end
-            if matched; return true, remainder end
+            newrems = union(newrems, remainders)
         end
 	end
-
-	return false, ""
-
+	return newrems
 end
 
 function validate(f::String)
@@ -60,8 +102,8 @@ function validate(f::String)
     rg = buildGraph(rules)
 
     function globalmatch(x)
-        (matched, remainder) = matchrule(x, rg, 1)
-        matched && (remainder == "")
+        remainders = matchrule(x, rg, 1)
+        "" in remainders
     end
 
     sum(globalmatch.(messages))
